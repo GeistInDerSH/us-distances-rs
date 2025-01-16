af8b05a483a36e74c9738efa07259cd6435ff7d8
Matthew Hill <GeistInDerSH@gmail.com>
2025-01-15 23:30:04 -0700

    Use Criterion for Benchmarking Code
    Try to get an idea of where the performance bottlenecks are for the existing code

---
+++ benches/benchmarks.rs
@@ -0,0 +1,18 @@
+#[macro_use]
+extern crate criterion;
+use criterion::Criterion;
+use usdist::{min_distance_for_point, try_load};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let points = try_load("points.txt").expect("Failed to load points from the file");
+    c.bench_function("haversine_distance_for_point", |b| {
+        b.iter(|| points[0].haversine_distance(&points[1024]))
+    });
+    c.bench_function("min_distance_for_point", |b| {
+        b.iter(|| min_distance_for_point(&points[0], &points))
+    });
+    c.bench_function("try_load_points", |b| b.iter(|| try_load("points.txt")));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
