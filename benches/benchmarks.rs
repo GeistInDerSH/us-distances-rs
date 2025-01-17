d0eaeefcf22554a96ac05a8562362485f5a8da9a
Matthew Hill <GeistInDerSH@gmail.com>
2025-01-17 12:59:21 -0700

    Update Benchmark Names to Match Functions

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -5,13 +5,13 @@ use usdist::{min_distance_for_point, try_load};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let points = try_load("points.txt").expect("Failed to load points from the file");
-    c.bench_function("haversine_distance_for_point", |b| {
+    c.bench_function("haversine_distance", |b| {
         b.iter(|| points[0].haversine_distance(&points[1024]))
     });
     c.bench_function("min_distance_for_point", |b| {
         b.iter(|| min_distance_for_point(&points[0], &points))
     });
-    c.bench_function("try_load_points", |b| b.iter(|| try_load("points.txt")));
+    c.bench_function("try_load", |b| b.iter(|| try_load("points.txt")));
 }
 
 criterion_group!(benches, criterion_benchmark);
